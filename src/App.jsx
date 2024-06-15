b54b99ad59f50cb23aface4db5fe63beffab7faa
Stu <stumac1000@yahoo.com>
2024-06-15 14:56:56 -0700

    Finish Lesson 1.1 Cover: Initializing project, npm scripts, React components, JSX, Linting, Lists

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,44 @@
-import * as React from "react";
-
-const title = "React";
+const list = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
 function App() {
   return (
     <div>
-      <h1>Howdy React</h1>
+      <h1>My Hacker Stories</h1>
+
+      <label htmlFor="search">Search: </label>
+      <input type="text" id="search" />
+      <hr />
+
+      <ul>
+        {list.map((item) => (
+          <li key={item.objectID}>
+            <h2>{item.title}</h2>
+            <a href={item.url} target="_blank">
+              Visit {item.title} Site
+            </a>
+            <p>Authors: {item.author}</p>
+            <p>Comments: {item.num_comments}</p>
+            <p>Points: {item.points}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
